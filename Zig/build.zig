9fc02118b93ab5af9a36f544068b378c628ff106
Maverik <Maverik@duck.com>
2024-12-07 06:12:55 +0000

    Challenge 1.1

---
+++ Zig/build.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{ .default_target = .{ .abi = .msvc, .cpu_arch = .x86_64, .os_tag = .windows } });
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+    
+    const exe = b.addExecutable(.{
+        .name = "aoc2024",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    
+    b.installArtifact(exe);
+
+    const exe_cmd = b.addRunArtifact(exe);
+    exe_cmd.step.dependOn(b.getInstallStep());
+
+    const exe_step = b.step("run", "Run executable");
+    exe_step.dependOn(&exe_cmd.step);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
